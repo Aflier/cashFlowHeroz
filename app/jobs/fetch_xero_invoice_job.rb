e80e3305d89b7f109bb0a2145a24ead3c1cfe9fb
Simon Kiteley <simon@aflier.com>
2026-06-11 16:35:13 +0100

    initial commit

--- app/jobs/fetch_xero_invoice_job.rb
+++ app/jobs/fetch_xero_invoice_job.rb
@@ -43,7 +43,7 @@ class FetchXeroInvoiceJob < ApplicationJob
     Rails.logger.info "Contact Person: #{invoice.contact.name}"
     Rails.logger.info "Invoice Total: #{invoice.total}"
 
-    token_record.transactions.find_or_create_by(why: invoice.invoice.Number) do |transaction|
+    token_record.transactions.find_or_create_by(why: invoice.invoice_number) do |transaction|
       transaction.transaction_at = Date.today
       transaction.amount = invoice.total
     end
