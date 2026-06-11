c67dcac4ca4b3bbb2771b5b15e6ab6dec387ab91
Simon Kiteley <simon@aflier.com>
2026-06-11 12:59:19 +0100

    initial commit

---
+++ app/jobs/xero_webhook_job.rb
@@ -0,0 +1,22 @@
+# app/jobs/xero_webhook_job.rb
+class XeroWebhookJob < ApplicationJob
+  queue_as :default
+
+  def perform(event)
+    resource_id = event["resourceId"]     # e.g., Invoice ID or Contact ID
+    event_type  = event["eventType"]      # e.g., "Create", "Update"
+    category    = event["eventCategory"]  # e.g., "INVOICE", "CONTACT"
+
+    case category
+    when "INVOICE"
+
+      puts ">>>>>>>>> INVOICE #{event}"
+
+
+      # Implement your system sync logic here
+      # (e.g., fetch the full invoice using the xero-ruby SDK)
+    when "CONTACT"
+      # Sync updated contact info
+    end
+  end
+end
