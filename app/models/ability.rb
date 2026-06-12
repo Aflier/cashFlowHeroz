f1885c47641aa8e634f409f36baa2f901d6fd98c
Simon Kiteley <simon@aflier.com>
2026-06-12 10:52:39 +0100

    initial commit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,44 +8,44 @@ class Ability
       can :manage, :all
     else
 
-      can [:index], LegalEntity
-      can [:index], Mission
-      can [:connector_preview, :update_connecting, :refresh_connection, :remove_connector,
-           :update_connector], User do |user__observing|
+      can [ :index ], LegalEntity
+      can [ :index ], Mission
+      can [ :connector_preview, :update_connecting, :refresh_connection, :remove_connector,
+           :update_connector ], User do |user__observing|
         user.id == user__observing.id
       end
       # LegalEntity permissions: responsible and contributor can view
-      can [:show], LegalEntity do |legal_entity|
-        legal_entity.entity_users.find_by(user: user, role: [:responsible, :contributor])
+      can [ :show ], LegalEntity do |legal_entity|
+        legal_entity.entity_users.find_by(user: user, role: [ :responsible, :contributor ])
       end
 
-      can [:update], EntityUser do |entity_user|
-        entity_user.legal_entity.entity_users.find_by(user: user, role: [:responsible, :contributor])
+      can [ :update ], EntityUser do |entity_user|
+        entity_user.legal_entity.entity_users.find_by(user: user, role: [ :responsible, :contributor ])
       end
 
       # Mission permissions: responsible and contributor can view and update
-      can [:show, :update], Mission do |mission|
-        mission.mission_users.find_by(user: user, role: [:responsible, :contributor])
+      can [ :show, :update ], Mission do |mission|
+        mission.mission_users.find_by(user: user, role: [ :responsible, :contributor ])
       end
 
       # MissionUser permissions: responsible and contributor can add new users to mission
-      can [:new, :create], MissionUser do |mission_user|
-        mission_user.mission.mission_users.find_by(user: user, role: [:responsible, :contributor])
+      can [ :new, :create ], MissionUser do |mission_user|
+        mission_user.mission.mission_users.find_by(user: user, role: [ :responsible, :contributor ])
       end
 
       # MissionUser permissions: only responsible can update user roles
-      can [:update], MissionUser do |mission_user|
-        mission_user.mission.mission_users.find_by(user: user, role: [:responsible, :contributor])
+      can [ :update ], MissionUser do |mission_user|
+        mission_user.mission.mission_users.find_by(user: user, role: [ :responsible, :contributor ])
       end
 
-      #Transaction permissions: responsible and contributor can create
-      can [:new, :create], Transaction do |transaction|
-        transaction.mission.mission_users.find_by(user: user, role: [:responsible, :contributor])
+      # Transaction permissions: responsible and contributor can create
+      can [ :new, :create, :show ], Transaction do |transaction|
+        transaction.mission.mission_users.find_by(user: user, role: [ :responsible, :contributor ])
       end
 
       # Transaction permissions: responsible and contributor can modify
-      can [:edit, :update, :destroy], Transaction do |transaction|
-        transaction.mission.mission_users.find_by(user: user, role: [:responsible, :contributor])
+      can [ :edit, :update, :destroy ], Transaction do |transaction|
+        transaction.mission.mission_users.find_by(user: user, role: [ :responsible, :contributor ])
       end
 
     end
